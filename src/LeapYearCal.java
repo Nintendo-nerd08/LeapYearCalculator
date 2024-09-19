bb237aae149326308ea8a7bc490404eda0cbf4d8
lloydkev000 <lloydkevin631@gmail.com>
2024-09-19 08:26:38 -0400

    All Done!

--- src/LeapYearCal.java
+++ src/LeapYearCal.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class LeapYearCal {
+public class LeapYearCal {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
+    }
+
+    private static boolean isLeapYear(int year) {
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                return year % 400 == 0;
+            }else{
+                return true;
+            }
+        }else{
+            return false;
+        }
+
+    }
+
+
 }
